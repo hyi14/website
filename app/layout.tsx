e1125f879e8bb7e62d51fc0d9e752ef8febb0eed
Hannah <114905482+hyi14@users.noreply.github.com>
2025-08-22 23:30:03 -0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,22 +15,6 @@ export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: 'Hannah Yi',
   description: 'This is my Website.',
-  url: baseUrl,
-  siteName: 'My Portfolio',
-  locale: 'en_US',
-  type: 'website',
-},
-robots: {
-  index: true,
-  follow: true,
-  googleBot: {
-    index: true,
-    follow: true,
-    'max-video-preview': -1,
-    'max-image-preview': 'large',
-    'max-snippet': -1,
-    },
-  },
 }
 
 const cx = (...c: string[]) => c.filter(Boolean).join(' ')
