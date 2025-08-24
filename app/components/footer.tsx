c36d3051767873c42ece88effc4164ca8533e1da
Hannah <114905482+hyi14@users.noreply.github.com>
2025-08-24 16:53:48 -0700

    Update footer.tsx

--- app/components/footer.tsx
+++ app/components/footer.tsx
@@ -19,7 +19,7 @@ export default function Footer() {
   return (
     <footer className="mb-16">
       <p className="mt-8 text-neutral-600 dark:text-neutral-300">
-        © {new Date().getFullYear()} Always Assessing
+        © {new Date().getFullYear()} Hannah Davis Yi
       </p>
     </footer>
   )
