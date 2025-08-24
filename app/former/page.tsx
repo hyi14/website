2a4afc105dec2321950e8f0ae2fbc7fbac9bee03
Hannah <114905482+hyi14@users.noreply.github.com>
2025-08-24 03:44:43 -0700

    Update page.tsx

--- app/former/page.tsx
+++ app/former/page.tsx
@@ -5,15 +5,15 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <section>
+    <section className="prose">
         <ul>
-          <li>• UW - Informatics Teaching Assistant</li>
-          <li>• Develop for Good ChangeArts - Design Manager</li>
-          <li>• HPE - Student Consultant</li>
-          <li>• Women In Informatics (WINFO) - President, Director of Community Efforts</li>
-          <li>• Poke Bay - Product Designer</li>
-          <li>• Hosted University of Washington’s largest hackathon for secondary school students in low-income districts</li>
-          <li>• Hackathon Judge, Design Mentor</li>
+          <li>UW - Informatics Teaching Assistant</li>
+          <li>Develop for Good ChangeArts - Design Manager</li>
+          <li>HPE - Student Consultant</li>
+          <li>Women In Informatics (WINFO) - President, Director of Community Efforts</li>
+          <li>Poke Bay - Product Designer</li>
+          <li>Planned University of Washington’s largest hackathon for secondary school students in low-income districts</li>
+          <li>Hackathon Judge, Design Mentor</li>
         </ul>
     </section>
   )
