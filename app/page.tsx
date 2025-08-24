57f9834fbd0b802230b6526eb187d7e12d729e0d
Hannah <114905482+hyi14@users.noreply.github.com>
2025-08-24 03:36:58 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,11 @@ import { BlogPosts } from 'app/components/posts'
 export default function Page() {
   return (
     <section>
-      <p className="mb-4 prose">
+      <p className="mb-4">
         <ul>
-          <li>Independent Researcher on AI Governance</li>
-          <li>UW Cybersecurity Teaching Assistant - cultivating next generation of cybersecurity leaders</li>
-          <li>iSchool Research Assistant - Creative ownership and digital remixing</li>
+          <li>• Independent Researcher on AI Governance</li>
+          <li>• UW Cybersecurity Teaching Assistant - cultivating next generation of cybersecurity leaders</li>
+          <li>• UW iSchool Research Assistant - Creative ownership and digital remixing</li>
         </ul>
       </p>
       <div className="my-8">
