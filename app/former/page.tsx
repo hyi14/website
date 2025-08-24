dd0dbbfe02163a3cad60d33c369a79b2755d38ed
Hannah <114905482+hyi14@users.noreply.github.com>
2025-08-23 18:14:35 -0700

    Create page.tsx

---
+++ app/former/page.tsx
@@ -0,0 +1,20 @@
+export const metadata = {
+  title: ‘Former’,
+  description: ‘Discover my past work.’,
+}
+
+export default function Page() {
+  return (
+    <section>
+      <p className="mb-4">
+        <ul>
+          <li>Point 1</li>
+          <li>Point 2</li>
+          <li>Point 3</li>
+          <li>Point 4</li>
+          <li>Point 5</li>
+        </ul>
+      </p>
+    </section>
+  )
+}
