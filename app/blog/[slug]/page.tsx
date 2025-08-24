b9f8f6f30cb5ce2ae9f318797c11bf9f29c489cb
Hannah <114905482+hyi14@users.noreply.github.com>
2025-08-24 04:20:07 -0700

    Update page.tsx

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -90,7 +90,7 @@ export default function Blog({ params }) {
           {formatDate(post.metadata.publishedAt)}
         </p>
       </div>
-      <article className="prose-lg">
+      <article className="prose prose-lg">
         <CustomMDX source={post.content} />
       </article>
     </section>
