efb9a03ec38e48ef83ac854801bb3d6fffdcd4ca
Hannah <114905482+hyi14@users.noreply.github.com>
2025-08-24 03:46:03 -0700

    Create page.tsx

---
+++ app/other/page.tsx
@@ -0,0 +1,20 @@
+export const metadata = {
+  title: 'Other',
+  description: 'Discover other information.',
+}
+
+export default function Page() {
+  return (
+    <section className ="prose">
+        <ul>
+          <li>cool facts</li>
+          <li>here</li>
+          <li>and</li>
+          <li>there</li>
+          <li>not</li>
+          <li>things I did but</li>
+          <li>what made me</li>
+        </ul>
+    </section>
+  )
+}
