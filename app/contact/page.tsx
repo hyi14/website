62381e359ba00df404639d527ce4a2958fbf4c9c
Hannah <114905482+hyi14@users.noreply.github.com>
2025-08-23 17:38:28 -0700

    Create page.tsx

---
+++ app/contact/page.tsx
@@ -0,0 +1,18 @@
+export const metadata = {
+  title: 'Contact',
+  description: 'Get in touch with me.',
+}
+
+export default function ContactPage() {
+  return (
+    <section>
+      <p>
+        <a href="hannahyi@uw.edu">[Email]</a>
+        <i>Transform chaos into clear patterns, lasting protection
+          Move in systems to reduce noice, reveal patterns
+          Move in systems to limit chaos, liberate clarity
+        </i> 
+      </p>
+    </section>
+  )
+}
